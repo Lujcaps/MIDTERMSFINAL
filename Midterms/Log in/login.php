ab2ab0dfc0ed12142bb702616240b1646c21ce00
Lujcaps <111631892+Lujcaps@users.noreply.github.com>
2022-11-05 12:37:03 +0800

    Add files via upload

--- Midterms/Log in/login.php
+++ Midterms/Log in/login.php
@@ -3,135 +3,100 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="Style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Static login</title>
 </head>
 <body>
-<?php
-session_start();
-    if(isset($_POST['signinBtn']) && isset($_POST['UserType'])){
-        $arrUtype = $_POST['UserType'];
 
-        foreach ($arrUtype as $key => $Utypevalue){
-            if(($Utypevalue == "admin")){
-                
-                $admin1 ="admin";
-                $Padmin1 ="pass1234";
-                $admin2 = "renmark";
-                $Padmin2 = "pog1234";
-                
-    
-                if(($_POST['Username'] == $admin1 && $_POST['Password']==$Padmin1))
-                {
-                    $_SESSION['Username'] = $admin1;
-
-
-                    echo '<div class="alert alert-success w-50 p-3 mx-auto">
-                    welcome to the system ',$admin1,
-                   '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                }
-                elseif(($_POST['Username'] == $admin2 && $_POST['Password']==$Padmin2)){
-                        $_SESSION['Username'] = $admin2;
-                        echo '<div class="alert alert-success w-50 p-3 mx-auto">
-                        welcome to the system ',$admin2,
-                       '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>'; 
-                    }
-                    else
-                    {
-                    echo '<div class="alert alert-danger w-50 p-3 mx-auto">
-                    You Entered the wrong password
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                    }         
-                }
-
-
-                if(($Utypevalue == "ContentManager")){
-                
-                    $ContentManager1 ="pepito";
-                    $PcontentManager1 ="manaloto";
-                    $ContentManager2 = "juan";
-                    $PcontentManager2 = "delacruz";
-                    
-        
-                    if(($_POST['Username'] == $ContentManager1 && $_POST['Password']==$PcontentManager1))
-                    {
-                        $_SESSION['Username'] = $ContentManager1;
-    
-    
-                        echo '<div class="alert alert-success w-50 p-3 mx-auto">
-                        welcome to the system ',$ContentManager1,
-                       '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                    }
-                    elseif(($_POST['Username'] == $ContentManager2 && $_POST['Password']==$PcontentManager2)){
-                            $_SESSION['Username'] = $ContentManager2;
-                            echo '<div class="alert alert-success w-50 p-3 mx-auto">
-                            welcome to the system ',$ContentManager2,
-                           '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>'; 
-                        }
-                        else
-                        {
-                        echo '<div class="alert alert-danger w-50 p-3 mx-auto">
-                        You Entered the wrong password
-                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                        }         
-                    }
-
-                    if(($Utypevalue == "SystemUS")){
-                
-                        $SystemUS1 ="pedro";
-                        $PSystemU1 ="penduko";
-                        
-                        
-            
-                        if(($_POST['Username'] == $SystemUS1 && $_POST['Password']==$PSystemU1))
-                        {
-                            $_SESSION['Username'] = $SystemUS1;
-        
-        
-                            echo '<div class="alert alert-success w-50 p-3 mx-auto">
-                            welcome to the system ',$SystemUS1,
-                           '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                        }
-                            else
-                            {
-                            echo '<div class="alert alert-danger w-50 p-3 mx-auto">
-                            You Entered the wrong password
-                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> </div>';
-                            }         
-                        }
-    
-
-            }
-        }
-    ?>
-
-
+<div id="login">
+  
 <div class="container">
         <div class="card card-container">
-            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+        
+            <img id="profile img" height="150" width="150" src="prof1.jfif"/>
             <p id="profile-name" class="profile-name-card"></p>
             <form class="form-signin" method="post">
-            <div class="form-row align-items-center">
-            <div class="col-auto my-1 ">
-            <select class="form-control" id="UType" name ="UserType[]">
-                <option value="admin">Admin</option>
-                <option value="ContMan">Content Manager</option>
-                <option value="SystemUs">System User</option>
-            </select>
-            </div>
+                <span id="reauth-email" class="reauth-email"></span>
+            
+                <select name="userSelect"  class="form-control" id="userSelect">
+                    <option value="Admin">Admin</option>
+                    <option value="Content Manager">Content Manager</option>
+                    <option value="System Users">System Users</option>
+                </select>
                 <br>
-                <input type="text" name="Username" id="username" class="form-control" placeholder="User" required autofocus>
-                <input type="password" name="Password"  id="password" class="form-control" placeholder="Password" required>
-                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit" name="signinBtn">Sign in</button>
+
+                <input type="text" name="userName" id="inputEmail" class="form-control" placeholder="User Name" required autofocus>
+                <input type="text"  name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+             
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit" name="Signin">Sign in</button>
             </form>
+
+            <?php
+
+                $position = array("Admin","Content Manager","System Users");
+                $users = array(
+                    array(
+                    "Username" => "renmark",
+                    "Password" => "Pogi1234"
+                    ),
+                    array(
+                    'Username' => 'admin',
+                    'Password' => 'Pass1234'                            
+                    ),
+                    array(
+                        'Username' => 'pepito',
+                        'Password' => 'manaloto'                           
+                    ),
+                    array(
+                        'Username' => 'juan',
+                        'Password' => 'delacruz'
+                        
+                    ),
+                    array(
+                        'Username' => 'pedro',
+                        'Password' => 'penduko'           
+                    )
+                    
+                   );
+
+                   if (isset($_POST['Signin'])) {
+
+                    $user = $_POST['userSelect'];
+                    $username = $_POST['userName'];
+                    $password = $_POST['password'];
+
+                    if ($user === $position[0] && $username === $users[0]['Username'] && $password === $users[0]['Password']) {
+                        echo '<div class="alert alert-success" role="alert">welcome to the system: '.  $users[0]['Username']  .'</div>';
+                        
+                    }
+                    elseif ($user === $position[0] && $username === $users[1]['Username'] && $password === $users[1]['Password']){
+                        echo '<div class="alert alert-success" role="alert">welcome to the system: '.  $users[1]['Username']  .'</div>';
+                    }
+                    elseif ($user === $position[1] && $username === $users[2]['Username'] && $password === $users[2]['Password']){
+                        echo '<div class="alert alert-success" role="alert">welcome to the system: '.  $users[2]['Username']  .'</div>';
+                    }
+                    elseif ($user === $position[1] && $username === $users[3]['Username'] && $password === $users[3]['Password']){
+                        echo '<div class="alert alert-success" role="alert">welcome to the system: '.  $users[3]['Username']  .'</div>';
+                    }
+                    elseif ($user === $position[2] && $username === $users[4]['Username'] && $password === $users[4]['Password']){
+                        echo '<div class="alert alert-success" role="alert">welcome to the system: '.  $users[4]['Username']  .'</div>';
+                    }         
+                    else{
+                        echo '<div class="alert alert-danger" role="alert">Incorrect Username/Password</div>';
+
+                    }
+                 }
+            ?>
+
+           
         </div>
     </div>
 
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
-
-
 </body>
 </html>
--- Midterms/vendo machine/vendo.php
+++ Midterms/vendo machine/vendo.php
@@ -1,272 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Style.css">
-    <title>Vendo machine</title>
-</head>
-<body>
-<h1>Vendo Machine</h1>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Vendo Machine</title>
+   </head>
+   <body>
+      <h2>Vendo Machine</h2>
+      
+      <?php
+         $arrDrinks = array('Coke' => 15, 'Sprite' => 20, 'Royal' => 20, 'Pepsi' => 15, 'Mountain Dew' => 20,);
+         $arrSizes = array('Regular Size' => 'Regular', 'Up-size (add ₱5)' => 'Up-size', 'Jumbo (add ₱10)' => 'Jumbo');
+         ?>
 
-<form method = "get">
- <fieldset class="fieldset">
-  <legend >Product</legend>
-  <input type="checkbox" name="sodas[]" id="sodasCoke" value = "Coke" > <label for = "sodasCoke">Coke - ₱15</label><br>
-  <input type="checkbox" name="sodas[]" id="sodasSprite" value = "Sprite"> <label for = "sodasSprite">Sprite - ₱20</label><br>
-  <input type="checkbox" name="sodas[]" id="sodasRoyal" value = "Royal"> <label for = "sodasRoyal">Royal - ₱20</label><br>
-  <input type="checkbox" name="sodas[]" id="sodasPepsi" value = "Pepsi"> <label for = "sodasPepsi">Pepsi - ₱15</label><br>
-  <input type="checkbox" name="sodas[]" id="sodasMountaindew" value = "Mountaindew"> <label for = "sodasMountaindew">Mountain Dew - ₱20</label><br>
-  
-</fieldset>
-<fieldset class="fieldset">
-  <legend>Options</legend>
-  <label for="size">Size:</label>
-<select name="size[]" id="sizes">
-  <option value="Regular">Regular </option>
-  <option value="UpSize">Up-Size (add ₱5)</option>
-  <option value="Jumbo">Jumbo (add ₱10)</option>
-</select>
+      <form method="POST">
+         <fieldset style="width: 370px;">
+            <legend>Products:</legend>
+            <?php
+               if(isset($arrDrinks)){
+                   foreach ($arrDrinks as $Dkey => $Dvalue) {
+                       echo "<input type='checkbox' name='Dcheckbox[". $Dkey ."]' value='". $Dvalue ."'>
+                       <label>". $Dkey. " - ₱". $Dvalue."</label><br>";
+                   }
+               }
+               ?>
+         </fieldset>
 
-<label for="QtySoda"> Quantity: </label>
-<input type="number" name="QtySoda" id="QtySoda" min="1" max="9999">
-<input type="submit" name="CheckOutbtn" value ="Check Out">
-</fieldset>
-</form>
-
-<?php
- if(isset($_GET['CheckOutbtn'])) {
-    if(empty($_GET['sodas'])){
-        echo '<hr>';
-        echo '<b>No Selected Product, Try Again</b>';
-    }
-    }
-error_reporting(0);
-if(isset($_GET['CheckOutbtn']) &&
-       isset($_GET['sodas']) && 
-       isset($_GET['size']) && 
-       isset($_GET['QtySoda'])){
-echo '<hr>';
-// echo '<b>No Selected Product, Try Again</b> <br>';
-echo '<b>Purchase Summary:</b> <br>';
-
-        $arrSoda = $_GET['sodas'];
-        $arrSize = $_GET['size'];
-        $SodaQty = $_GET['QtySoda'];
-
-        foreach($arrSoda as $key => $SodaValue){
-            foreach($arrSize as $key => $SizeValue){
-
-                if (($SodaValue == "Coke")&&($SizeValue == "Regular")){
-                    $coke = 15 * $SodaQty;
-                    $QtyCoke = $SodaQty;
-
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }      
-                                  
+         <fieldset style="display: inline-block;">
+            <legend>Options:</legend>
+            <label for="Selectsize">Size</label>
+            <select name="Selectsize" id="Selectsize">
+            <?php
+               if (isset($arrSizes)) {
+                   foreach ($arrSizes as $Skey => $Svalue) {
+                       echo "<option value='". $Svalue."'>". $Skey."</option>";
+                   }
                 }
-              
-               
+               ?>
+            </select>
 
+            <label for="Quantity">Quantity</label>
+            <input type="number" name="Quantity" id="Quantity" min="0" max="100" value="0">
+            <button type="submit" name="Process">Check Out</button>
+         </fieldset>
+      </form>
+      <?php
+         if (isset($_POST['Process'])) {
+         
+             if(isset($_POST['Dcheckbox']) and isset($_POST['Selectsize'])){
+         
+                 $Qty = $_POST['Quantity'];
+                 $size = $_POST['Selectsize'];
+                 $process = $_POST['Dcheckbox'];
 
-                if (($SodaValue == "Sprite")&&($SizeValue == "Regular")){
-                    $sprite = 20 * $SodaQty;
-                    $QtySprite = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '&#9; <li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }      
-                                 
-                }
-
-
-                if (($SodaValue == "Royal")&&($SizeValue == "Regular")){
-                    $royal = 20 * $SodaQty; 
-                    $QtyRoyal = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }    
-                                   
-                }
-                if (($SodaValue == "Pepsi")&&($SizeValue == "Regular")){
-                    $pepsi = 15 * $SodaQty;
-                    $QtyPepsi = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }    
-                                      
-                }
-                if (($SodaValue == "Mountaindew")&&($SizeValue == "Regular")){
-                    $MountainDew = 20 * $SodaQty;
-                    $QtyMountaindew = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }    
-                     
-                }
-
-                
-                if (($SodaValue == "Coke")&&($SizeValue == "UpSize")){
-                    $coke = 20*$SodaQty;
-                    $QtyCoke = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }    
-                                      
-                }
-
-
-                if (($SodaValue == "Sprite")&&($SizeValue == "UpSize")){
-                    $sprite =25*$SodaQty; 
-                    $QtySprite = $SodaQty;
-
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }   
-                                 
-                }
-                if (($SodaValue == "Royal")&&($SizeValue == "UpSize")){
-                    $royal = 25*$SodaQty; 
-                    $QtyRoyal = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }   
-                                      
-                }
-                if (($SodaValue == "Pepsi")&&($SizeValue == "UpSize")){
-                    $pepsi = 20 * $SodaQty;
-                    $QtyPepsi = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }   
-                                         
-                }
-                if (($SodaValue == "Mountaindew")&&($SizeValue == "UpSize")){
-                    $MountainDew = 25 * $SodaQty;
-                    $QtyMountaindew = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }   
-                       
-                }
-
-                if (($SodaValue == "Coke")&&($SizeValue == "Jumbo")){
-                    $coke = 25 * $SodaQty; 
-                    $QtyCoke = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$coke,'</li> <br>';
-                    }   
-                                    
-                }
-                if (($SodaValue == "Sprite")&&($SizeValue == "Jumbo")){
-                    $sprite = 30 * $SodaQty;
-                    $QtySprite = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$sprite,'</li> <br>';
-                    }   
-                                  
-                }
-                if (($SodaValue == "Royal")&&($SizeValue == "Jumbo")){
-                    $royal= 30 * $SodaQty; 
-                    $QtyRoyal = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$royal,'</li> <br>';
-                    }   
-                                      
-                }
-                if (($SodaValue == "Pepsi")&&($SizeValue == "Jumbo")){
-                    $pepsi= 25 * $SodaQty;
-                    $QtyPepsi = $SodaQty; 
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$pepsi,'</li> <br>';
-                    }   
-                                      
-                }
-                if (($SodaValue == "Mountaindew")&&($SizeValue == "Jumbo")){
-                    $MountainDew = 30 * $SodaQty;
-                    $QtyMountaindew = $SodaQty;
-                    if($SodaQty > 1){
-                        echo '<li> ',$SodaQty,' ','pieces of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }
-                    else
-                    {
-                    echo '<li> ',$SodaQty,' ','piece of ',$SizeValue,' ',$SodaValue,' amounting to ',' ₱ ',$MountainDew,'</li> <br>';
-                    }   
-                      
-                }
-            }
-        }
-        
-
-        $TotalAmnt = $coke + $sprite + $royal + $pepsi + $MountainDew;
-        $TotalSoda = $QtyCoke + $QtySprite + $QtyRoyal + $QtyPepsi +  $QtyMountaindew;
-        echo '<b>Total Number of Items: </b>',$TotalSoda,'<br>';
-        echo '<b>Total Amount: </b>',$TotalAmnt ,'<br>';
-        
-    }
-
-   
-
-    
-
-?>
-
-</body>
+                 if ($Qty == 1) {
+                     $set = "piece";   
+                 }
+                 else{
+                     $set = "pieces";   
+                 }
+         
+                 if ($size == 'Regular') {
+                     $addons = 0;   
+                 }
+                 elseif ($size == 'Up-size') {
+                     $addons = 5;  
+                 }
+                 elseif ($size == 'Jumbo') {
+                     $addons = 10;  
+                 }
+                 echo '<hr>';
+                 echo "<h3>Purchase Summary</h3>";
+                 
+                 foreach ($process as $Pkey => $Pvalue) {
+                     echo
+                     "<ul>
+                         <li>" . $Qty ."  ". $set ." of ". $size ." ". $Pkey ." amounting to ₱" . $Totalval =
+                         intval($Pvalue) * intval($Qty) + ($addons * intval($Qty)) .
+                         "</li>
+                     </ul>";
+                 }
+                     $Totalitems = ($Qty * sizeof($process));
+                     $grandTotal = (array_sum($process) + $addons * $Qty) * $Qty;
+         
+                     echo "<label><b>Total number of items:</b> </label>". $Totalitems ."<br>";
+                     echo "<label><b>Total number amount:</b> </label>". $grandTotal;
+                    
+             }
+             else{
+                echo '<hr>';
+                echo "No Selected product; please try again.";
+             }
+         }
+         ?>
+   </body>
 </html>
